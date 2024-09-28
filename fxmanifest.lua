5a0963926747d4d560aaa71b3e1f714c8dddecfa
ORS-Development <owencrumplin06@icloud.com>
2024-09-28 20:48:32 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'ors-clothingbag'
+author 'ORS-Development'
+
+version '1.0.0'
+
+server_scripts{
+    'server.lua',
+}
+
+client_scripts{
+    'client.lua',
+}
